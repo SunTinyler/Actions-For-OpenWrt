d5f2589e06fdde4026464f896e22a37a44807a58
shake <shake.chen@gmail.com>
2020-03-03 21:53:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_gene
 #git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 
 
-#remove default theme https://cuiqingwei.github.io/2019/12/24/2019-12-24-%E4%BD%BF%E7%94%A8GitHub-Actions%E7%BC%96%E8%AF%91OpenWRT/
+#remove default theme 
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 
