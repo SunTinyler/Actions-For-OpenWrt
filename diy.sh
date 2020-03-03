3b6e66bfe65ba3dc5c69fd4388d92497584678b8
shake <shake.chen@gmail.com>
2020-03-03 13:54:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,9 +8,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_generate
 
-#拦截广告的app
-
-cd package
-mkdir openwrt-packages
-cd openwrt-packages
-git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
+#拦截广告koolproxyR
+#cd package
+#mkdir openwrt-packages
+#cd openwrt-packages
+#git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
