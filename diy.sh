6b1234197deff2455f148b7455590f88d9f33e2b
shake <shake.chen@gmail.com>
2020-03-03 21:52:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,11 @@
 sed -i 's/192.168.1.1/192.168.199.10/g' package/base-files/files/bin/config_generate
 
 #拦截广告koolproxyR
-#cd package
-#mkdir openwrt-packages
-#cd openwrt-packages
-#git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
+#git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
+#git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+
+
+#remove default theme
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+
